73f3a3ce4760267fbc5aff2ce508bb8688978912
Alex Pan <alexpaofficial@gmail.com>
2022-10-04 09:49:11 -0700

    remove solution on jake branch

--- src/game.js
+++ src/game.js
@@ -1,10 +1,5 @@
 function Game(dimension) {
-	if (dimension < 3) {
-        throw new RangeError('Dimension must be greater than 3');
-    }
-    this.dimension_ = dimension;
-    this.board_ = this.getBoard(dimension);
-    this.player_ = this.PLAYER_ID.PLAYER_ONE;
+
 }
 
 Game.prototype.getBoard = function(dimension) {
@@ -25,40 +20,11 @@ Game.prototype.PLAYER_ID = {
     PLAYER_TWO: 1
 }
 
-Game.prototype.getRowAndColByTileId = function(id) {
-	let parts = id.split('-');
-    console.log(parts);
-    if (parts.length !== 3) {
-        console.log('WARNING - did not find 3 parts in tile id');
-        return [-1,-1];
-    } else {
-        return [parseInt(parts[1]), parseInt(parts[2])];
-    }
-}
-
 /**
  * @param{!Element} tile - The tile that was clicked
  */
 Game.prototype.handleClickOnTile = function(tile) {
-  const rowAndCol = this.getRowAndColByTileId(tile.id);
-  const row = rowAndCol[0];
-  const col = rowAndCol[1];
-	if (this.player_ === this.PLAYER_ID.PLAYER_ONE) {
-  	this.board_[row][col] = this.PLAYER_ID.PLAYER_ONE;
-    const circle = document.createElement('div');
-    circle.className = 'circle';
-    tile.append(circle);
-    this.player_ = this.PLAYER_ID.PLAYER_TWO;
-  } else {
-  	this.board_[row][col] = this.PLAYER_ID.PLAYER_ONE;
-    const crossRight = document.createElement('div');
-    const crossLeft = document.createElement('div');
-    crossRight.className = 'crossRight';
-    crossLeft.className = 'crossLeft';
-    tile.append(crossRight);
-    tile.append(crossLeft);
-    this.player_ = this.PLAYER_ID.PLAYER_ONE;
-  }
+
 }
 
 /** 
@@ -78,29 +44,7 @@ Game.prototype.handleClickOnTile = function(tile) {
     @return {!Element} 
 */
 Game.getRootNodeForDimension = function(game) {
-    const dimension = game.dimension_;
-    if(dimension < 3) {
-        return null;
-    }
-    const root = document.createElement('div');
-    root.id = 'root'
-    for (let i = 0; i < dimension; ++i) {
-        const row = document.createElement('div');
-        row.id = `row-${i}`;
-        row.className = 'row';
-        for (let j = 0; j < dimension; ++j) {
-            const tile = document.createElement('div');
-        tile.id = `tile-${i}-${j}`;
-        tile.className = 'tile';
-        tile.addEventListener('click', (pointerEvent) => {
-            tile.style.pointerEvents = 'none';
-            game.handleClickOnTile(pointerEvent.srcElement);
-        });
-        row.append(tile);
-        }
-        root.append(row);
-    }
-    return root;
+    
 }
 
 module.exports = Game;
